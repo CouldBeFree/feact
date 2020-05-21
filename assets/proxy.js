ced192b91b58dedcbcb327165d738a517ab26ecb
couldbefree <zoriaalexandr@gmail.com>
2020-05-21 21:28:03 +0300

    proxy

--- assets/proxy.js
+++ assets/proxy.js
@@ -1,3 +1,39 @@
-const add = (a, b) => a + b;
-console.log(add(100, 58))
+/*const target = {
+	someProp: 1
+}
 
+const handler = {
+	get: function (target, key) {
+		return key in target ? target[key] : 'Doesnt exists'
+	}
+}
+const proxy = new Proxy(target, handler);
+console.log(proxy.someProp);*/
+
+/*const input = document.querySelector('#username');
+
+const inputState = {
+	id: 'username',
+	value: ''
+}
+
+const handler = {
+	set: function (target, key, value) {
+		if(target.id === 'username') {
+			target[key] = value;
+			document.querySelector(`#${target.id}`).value = value;
+			return true;
+		}
+		return false;
+	}
+}
+
+const proxy = new Proxy(inputState, handler);
+proxy.value = 'John Doe';
+
+input.addEventListener('input', (e) => {
+	proxy.value = e.target.value;
+	console.info('result', proxy.value, input.value);
+})
+
+console.info('result', proxy.value, input.value);*/
